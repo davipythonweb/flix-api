154d3f362019949ba8a061e169a4ab3f81c7d049
davipythonweb <galaxys3tab32@gmail.com>
2024-06-02 12:19:20 -0300

    create endpoint  get `views.py`

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from genres.models import Genre
 
-# Create your views here.
+
+def genre_view(request):
+    genres = Genre.objects.all()
+    data = [{'id': genre.id, 'name': genre.name} for genre in genres] # for na lista de dados , usando list-comprehension
+    return JsonResponse(data, safe=False)
