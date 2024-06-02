0e3caf77610bc9b25141022cbff84ee327d92383
davipythonweb <galaxys3tab32@gmail.com>
2024-06-02 01:54:43 -0300

    create new app `genres`

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 `python manage.py migrate`
 `python manage.py createsuperuser`
 `python manage.py runserver`
+`python manage.py startapp <nome_do_app>`
 
 `adm controller`
 
 *admsuper
 *Adm$50001
-
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
 
-def oi_view(request):
-    return JsonResponse({'message': 'Ola Mundo!'})
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', oi_view),
+    path('',),
 ]
