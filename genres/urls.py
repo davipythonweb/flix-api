18761de398e175efddc6a4749544ba0ecf605fd9
davipythonweb <galaxys3tab32@gmail.com>
2024-06-28 05:22:42 -0300

    update urls.py

--- genres/urls.py
+++ genres/urls.py
@@ -6,8 +6,6 @@ from . import views
 app_name = 'namespace'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
     path('genres/', views.GenereCreateLIstView.as_view(), name='list'),
     path('genres/<int:pk>/',views.GenereRetrieveUpdateDestroyView.as_view(), name='detail'),
     # path('genres/', views.genre_create_list_view, name='list'),
