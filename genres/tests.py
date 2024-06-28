ae07c6906848ad9fcb27e52b1943591db44663c4
davipythonweb <galaxys3tab32@gmail.com>
2024-06-28 05:46:28 -0300

    update project

--- genres/tests.py
+++ genres/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
-# Testando url genres de list 
+# # Testando url genres de list 
 class GenresURlsTeste(TestCase):
     def test_genres_list_create_is_url_correct(self):
         url = reverse('namespace:list')
         self.assertEqual(url, '/genres/')
             
-    # testando url genres de update e delete por id
+#     # testando url genres de update e delete por id
     def test_genres_update_delete_is_url_correct(self):
         url = reverse('namespace:detail', kwargs={'pk': 1})
         self.assertEqual(url, '/genres/1/')
