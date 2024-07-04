aa3a9909706f4677ba58403145610923af226faa
davipythonweb <galaxys3tab32@gmail.com>
2024-07-04 03:55:21 -0300

    create model `review`

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from movies.models import Movie
 
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Review(models.Model):
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.PROTECT,
+        related_name='reviews'
+    )
+    # poderia implementar um choices com uma lista com as estrelas possiveis.
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(0, 'Avaliação não pode ser inferior a 0 estrelas.'),
+            MaxValueValidator(5, 'Avaliação não pode ser superior a 5 estrelas.'),
+        ]
+    )
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.movie
