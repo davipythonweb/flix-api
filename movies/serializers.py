36246ea36b5e20df2df829c529a41fde99f4f1de
davipythonweb <galaxys3tab32@gmail.com>
2024-07-06 03:30:26 -0300

    criando view de estatisticas e atualizando serializer do app `movies`

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,6 +3,12 @@ from movies.models import Movie
 
 # from genres.models import Genre
 # from actors.models import Actor
+from django.db.models import Avg
+
+from actors.serializers import ActorSerializer
+from genres.serializers import GenreSerializer
+
+
 
 
 # criando serializer na mão, sem o [ModelSerializer] 
@@ -22,18 +28,78 @@ class MovieTesteSerializer(serializers.Serializer):
     resume = serializers.CharField()
 """
 
+# Serializer para o modelo Movie com todas as operações CRUD
 class MovieSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Movie
-        fields = '__all__'
-
+        fields = '__all__'  # Inclui todos os campos do modelo Movie
+    
+    # Validação personalizada para a data de lançamento
     def validate_release_date(self, value):
         if value.year < 1990:
             raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
         return value
     
+    # Validação personalizada para o resumo
     def validate_resume(self, value):
         if len(value) > 200:
             raise serializers.ValidationError('Resumo não deve ser maior do que 200 caracteres.')
         return value
+
+
+# Serializer detalhado para listagem de filmes, incluindo relações
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True)  # Serializa os atores relacionados
+    genre = GenreSerializer()  # Serializa o gênero relacionado
+    rate = serializers.SerializerMethodField()  # Campo calculado para a média das avaliações
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rate', 'resume']
+
+    # Método para obter a média das avaliações (rate)
+    def get_rate(self, obj):
+        average_rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        if average_rate:
+            return round(average_rate, 1)  # Retorna a média arredondada para uma casa decimal
+
+
+# Serializer para estatísticas de filmes
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()  # Campo para o total de filmes
+    movies_by_genre = serializers.ListField()  # Campo para a contagem de filmes por gênero
+    total_reviews = serializers.IntegerField()  # Campo para o total de avaliações
+    average_stars = serializers.FloatField()  # Campo para a média de estrelas das avaliações
+
+
+
+
+
+"""
+Explicação dos componentes principais:
+
+1-Imports:
+
+    serializers do Django REST framework para criar serializers.
+    Movie do aplicativo de filmes.
+    Avg do Django para calcular a média dos valores.
+    ActorSerializer e GenreSerializer para serializar relações de atores e gêneros.
+
+2-MovieSerializer:
+
+    Serializa todos os campos do modelo Movie.
+    Inclui validações personalizadas para a data de lançamento (release_date) e o resumo (resume).
+
+3-MovieListDetailSerializer:
+
+    Inclui campos relacionados (atores e gênero) usando serializers aninhados.
+    Adiciona um campo calculado (rate) para a média das avaliações.
+
+4-MovieStatsSerializer:
+
+    Define os campos para estatísticas de filmes, 
+    incluindo total de filmes, contagem de filmes por gênero,
+    total de avaliações e média de estrelas.
+
+"""
--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,99 @@
-from rest_framework import generics
+from django.db.models import Count, Avg
+from rest_framework import generics, views, response, status
 from movies.models import Movie
-from movies.serializers import MovieSerializer
+
+from movies.serializers import MovieSerializer, MovieListDetailSerializer, MovieStatsSerializer
+from reviews.models import Review
 
 # from movies.serializers import MovieTesteSerializer
 
-class MovieCreateListView(generics.ListCreateAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
 
+
+# Definição da view para criação e listagem de filmes
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()  # Consulta para obter todos os filmes
+    
+    # Método para determinar o serializer a ser usado com base no método HTTP
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer  # Serializer para listagem detalhada de filmes
+        return MovieSerializer  # Serializer para criação de filmes
+
+
+# Definição da view para recuperação, atualização e exclusão de um filme específico
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()  # Consulta para obter todos os filmes
+    
+    # Método para determinar o serializer a ser usado com base no método HTTP
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer  # Serializer para detalhamento do filme
+        return MovieSerializer  # Serializer para atualização e exclusão de filmes
+
+
+# View personalizada para obter estatísticas sobre os filmes e avaliações
+class MovieStatsView(views.APIView):
+    queryset = Movie.objects.all()  # Consulta para obter todos os filmes
+
+    def get(self, request):
+        total_movies = self.queryset.count()  # Contagem total de filmes
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))  # Contagem de filmes por gênero
+        total_reviews = Review.objects.count()  # Contagem total de avaliações
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']  # Média de estrelas das avaliações
+
+        # Dados a serem enviados na resposta
+        data = {
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+        
+        # garante que o valor seja arredondado para uma casa decimal e,
+        # se não houver avaliações (average_stars for None), retorna 0.
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }
+        
+
+        # Serialização dos dados
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)  # Validação dos dados serializados
+
+        return response.Response(
+            data=serializer.data,  # Dados serializados na resposta
+            status=status.HTTP_200_OK,  # Código de status HTTP 200 (OK)
+        )
+    
+
+
+
+"""
+Explicação dos componentes principais:
+
+1-Imports:
+
+    Count e Avg são funções de agregação do Django para contar registros e calcular a média.
+
+    generics e views são classes base do Django REST framework para criar views.
+
+    response e status são utilizados para criar respostas HTTP e definir códigos de status HTTP.
+
+    Movie e Review são modelos importados dos respectivos aplicativos.
+
+    MovieSerializer, MovieListDetailSerializer e MovieStatsSerializer são 
+    serializers para manipular os dados dos filmes.
+
+2-Views genéricas:
+
+    MovieCreateListView e MovieRetrieveUpdateDestroyView são 
+    views genéricas para operações CRUD em filmes, utilizando 
+    diferentes serializers dependendo do método HTTP.
+
+3-View personalizada MovieStatsView:
+
+    Coleta estatísticas sobre os filmes e avaliações.
+
+    Usa consultas e agregações para obter dados como contagem total de filmes,
+    contagem de filmes por gênero, total de avaliações e média de estrelas.
+
+    Serializa esses dados e os retorna em uma resposta HTTP 200 (OK).
+
+"""
