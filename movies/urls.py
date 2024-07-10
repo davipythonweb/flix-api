a2b3b04840dcb272a51da1ccfca5d04bc9575687
davipythonweb <galaxys3tab32@gmail.com>
2024-07-10 03:15:02 -0300

    create endpoint para stats  app movies `urls.py`

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ app_name = 'namespaceMovies'
 urlpatterns = [
     path('movies/', views.MovieCreateListView.as_view(), name='list'),
     path('movies/<int:pk>/',views.MovieRetrieveUpdateDestroyView.as_view(), name='retrieve'),
+     path('movies/stats/', views.MovieStatsView.as_view(), name='movie-stats-view'),
 ]
