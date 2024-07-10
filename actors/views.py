6cbe5d13fa9c30947fa28ccdb3d4c5f2584d7090
davipythonweb <galaxys3tab32@gmail.com>
2024-07-09 23:30:08 -0300

    comentario  app actors `views.py`

--- actors/views.py
+++ actors/views.py
@@ -8,11 +8,11 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,) # permissao para as views com acesso via token jwt
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,) # permissao para as views com acesso via token jwt
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
