9bb013347c596764cfd1cc598a8a5bdaccc6f6b4
davipythonweb <galaxys3tab32@gmail.com>
2024-07-10 01:21:30 -0300

    sobescrevendo configuraçoes no core settings.py `simple_jwt`

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,4 +129,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+
+# Sobescrevendo as configurções do simple-jwt
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
